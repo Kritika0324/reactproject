61809d97b8160cf697846f62683e4f9a21aa20f5
Kritika0324 <Kritikag926@gmail.com>
2022-09-13 22:21:55 +0530

    first

---
+++ src/components/common/tabOptions/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TabOptions = () => {
+    return (
+        <div>
+           This is tabOptions
+        </div>
+    )
+    
+}
+
+export default TabOptions
---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+import Header from "../../components/common/header";
+import TabOptions from "../../components/common/tabOptions";
+
+const HomePage = () => {
+    return (
+        <div>
+           <Header />
+           <TabOptions />
+        </div>
+    )
+}
+
+export default HomePage
